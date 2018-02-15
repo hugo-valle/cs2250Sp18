8ebdd1ca649cfb29b618e5a28c064339d2b5b9af
Hugo Vale <hugovalle1@weber.edu>
2018-02-15 10:11:34 -0700

    I

---
+++ mod6/hwModule6.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+// Constants
+const int PLAYERS = 5;
+const int ATTRI = 2;
+
+// Main Function
+int main()
+{
+    // 1.0) Define arrays for values
+    
+    // 1.1) Fill in values
+    
+    // Loop {
+    // 2.0) Display Menu
+    // MENU
+    // u - Update player rating
+    // a - Output players above a rating
+    // r - Replace player
+    // o - Output roster
+    // q - Quit
+    //
+    // Choose an option:
+    
+    // 3) Implement the "Output roster" menu option. 
+
+    // 4) Implement the "Update player rating" menu option.
+    //    Prompt the user for a player's jersey number.
+    //    Prompt again for a new rating for the player, 
+    //    and then change that player's rating.
+
+    // 5) Implement the "Output players above a rating" 
+    // menu option. Prompt the user for a rating. 
+    // Print the jersey number and rating for all players 
+    // with ratings above the entered value
+
+    // 6) Implement the "Replace player" menu option. 
+    // Prompt the user for the jersey number of the player
+    // to replace. If the player is in the roster, then 
+    // prompt again for a new jersey number and rating.
+    // Update the replaced player's jersey number and rating.
+    
+    // end of loop 
+
+    return 0;
+}
