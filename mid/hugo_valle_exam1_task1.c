7a167c4ebf91b7be8c375e5cfd29c85853c0b1fc
Hugo Vale <hugovalle1@weber.edu>
2018-02-27 08:44:29 -0700

    Initial commit

---
+++ mid/hugo_valle_exam1_task1.c
@@ -0,0 +1,32 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hugo_valle_exam1_task1.c
+ *
+ *    Description:  First Choice.
+ *
+ *        Version:  1.0
+ *        Created:  02/27/2018 08:42:02 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Hugo Vale (), hugovalle1@weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+
+    return 0;
+}
+// Function Definitions
+
+
---
+++ mid/hugo_valle_exam1_task2.c
@@ -0,0 +1,32 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hugo_valle_exam1_task2.c
+ *
+ *    Description:  Second choice
+ *
+ *        Version:  1.0
+ *        Created:  02/27/2018 08:42:25 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Hugo Vale (), hugovalle1@weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+
+    return 0;
+}
+// Function Definitions
+
+
