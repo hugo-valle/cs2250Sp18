983a5aa24fcf6289eaffe3e61bc71f4b04d50cc6
Hugo Valle <hugovalle1@weber.edu>
2018-03-20 10:03:47 -0600

    Initial commmit

---
+++ mod9/testMultiDudes.c
@@ -0,0 +1,37 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  testMultiDudes.c
+ *
+ *    Description:  Test multiple structures in functions
+ *
+ *        Version:  1.0
+ *        Created:  03/20/2018 09:37:30 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Hugo Valle (), hugovalle1@weber.edu
+ *   Organization:  WSU
+ *
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include "Dude.h"
+
+// Constants
+
+// Function Prototypes
+
+// Main Function
+int main()
+{
+    SuperDude s1;
+    // The & operator gives you the address
+    InitInfoStruct(&s1); // init struct info
+    ShowInfoStruct(&s1); // display struct info
+
+    return 0;
+}
+// Function Definitions
+
+
