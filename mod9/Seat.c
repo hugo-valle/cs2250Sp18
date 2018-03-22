849f3b7febf1335a1f62bde6a183031b23a9be3d
Hugo Valle <hugovalle1@weber.edu>
2018-03-22 09:49:23 -0600

    First requirement completed

--- mod9/Seat.c
+++ mod9/Seat.c
@@ -16,10 +16,57 @@
  * =====================================================================================
  */
 #include <stdio.h>
+#include <string.h>
 #include "Seat.h"
 
 // Constants
 
 // Function Definitions
+/*** Functions for Seat ***/
 
+void SeatMakeEmpty(Seat* seat) 
+{
+    strcpy((*seat).firstName, "empty");
+    strcpy((*seat).lastName,  "empty");
+    (*seat).amountPaid = 0;
 
+    return;
+}
+
+bool SeatIsEmpty(Seat seat) 
+{
+    return (strcmp(seat.firstName, "empty") == 0);
+}
+
+void SeatPrint(Seat seat) 
+{
+    printf("%s ", seat.firstName);
+    printf("%s, ", seat.lastName);
+    printf("Paid: %d\n", seat.amountPaid);
+
+    return;
+}
+/*** End functions for Seat ***/
+
+/*** Functions for array of Seat ***/
+void SeatsMakeEmpty(Seat seats[], int numSeats) 
+{
+    int i = 0;
+    for (i = 0; i < numSeats; ++i) {
+        SeatMakeEmpty(&seats[i]);
+    }
+    return;
+}
+
+void SeatsPrint(Seat seats[], int numSeats) 
+{
+    int i = 0;
+
+    for (i = 0; i < numSeats; ++i) {
+        printf("%d: ", i);
+        SeatPrint(seats[i]);
+    }
+
+    return;
+}
+/*** End functions for array of Seat ***/
--- mod9/Seat.h
+++ mod9/Seat.h
@@ -18,5 +18,20 @@
 #ifndef  SEAT__INC__
 #define  SEAT_INC__
 
+#include <stdbool.h>
+
+typedef struct Seat_struct 
+{
+    char firstName[50];
+    char lastName[50];
+    int  amountPaid;
+} Seat;
+
+void SeatMakeEmpty(Seat* seat); 
+bool SeatIsEmpty(Seat seat);
+void SeatPrint(Seat seat);
+void SeatsMakeEmpty(Seat seats[], int numSeats);
+void SeatsPrint(Seat seats[], int numSeats);
+
 #endif /* ----- #ifndef SEAT__INC__ ----- */
 
